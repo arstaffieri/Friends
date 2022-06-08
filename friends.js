3e1c023fa745a34f7eca248d60af4629a5ed081b
Angie Staffieri <angie.staffieri@gmail.com>
2022-06-08 15:39:02 -0600

    Add a new friend

--- friends.js
+++ friends.js
@@ -1,6 +1,10 @@
 var friend = ["Kate"]
+var other_friend = ["Nicki"]
 if (friend = "Kate"){
   console.log ("You are my best friend")
+}
+else if (other_friend = "Nicki") {
+console.log ("You are my other friend")
 } else {
   console.log ("I hate you")
 }
